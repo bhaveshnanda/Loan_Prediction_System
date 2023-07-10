fcfc3c08ab2a2f787ffb0717c537dc27a12150a7
Loan_Pred <nandabhavesh2003@gmail.com>
2023-07-10 13:53:58 +0530

    Initial commit

---
+++ flaskapp.py
@@ -0,0 +1,48 @@
+# save this as app.py
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('ML_Model1', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/prediction.html', methods=['GET', 'POST'])
+def predict():
+    if request.method ==  'POST':
+        Gender = int(request.form['Gender'])
+        Married = int(request.form['Married'])
+        Dependents = int(request.form['Dependents'])
+        Education = int(request.form['Education'])
+        Self_Employed = int(request.form['Self_Employed'])
+        Credit_History = int(request.form['Credit_History'])
+        Property_Area = int(request.form['Property_Area'])
+        ApplicantIncome = int(request.form['ApplicantIncome'])
+        CoapplicantIncome = int(request.form['CoapplicantIncome'])
+        LoanAmount = int(request.form['LoanAmount'])
+        Loan_Amount_Term = int(request.form['Loan_Amount_Term'])
+
+
+        
+        feature = [[Gender, Married,Dependents, Education, Self_Employed, ApplicantIncome, CoapplicantIncome, LoanAmount, Loan_Amount_Term, Credit_History, Property_Area]] 
+        print(feature)
+        prediction = model.predict(feature)
+
+        if prediction==1:
+            return render_template("prediction.html", prediction_text="Congratulations!! you are eligible for loan", prediction_color = "green")
+        
+        if prediction==0:
+            return render_template("prediction.html", prediction_text="Sorry, According to our Calculations, you will not get the loan", prediction_color = "red")
+
+
+
+
+    else:
+        return render_template("prediction.html")
+
+
+predict
+app.run(debug=True)
